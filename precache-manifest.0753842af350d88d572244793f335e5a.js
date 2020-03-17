9e8092ef3c67091997e83a16693da6c4126baf34
keldeo <lefrancpierreantoine@gmail.com>
2020-03-17 01:14:34 +0100

    Updates

---
+++ precache-manifest.0753842af350d88d572244793f335e5a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df66fd071e1312d61d1718272cd3d965",
+    "url": "/InstaZZ-front/index.html"
+  },
+  {
+    "revision": "e56f5ee9047a4611ea69",
+    "url": "/InstaZZ-front/static/css/2.b1f3d402.chunk.css"
+  },
+  {
+    "revision": "fdac2b3808dc0a222131",
+    "url": "/InstaZZ-front/static/css/main.9744d463.chunk.css"
+  },
+  {
+    "revision": "e56f5ee9047a4611ea69",
+    "url": "/InstaZZ-front/static/js/2.7b47d20c.chunk.js"
+  },
+  {
+    "revision": "b9f28c176efdd716457c02939f4eac67",
+    "url": "/InstaZZ-front/static/js/2.7b47d20c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdac2b3808dc0a222131",
+    "url": "/InstaZZ-front/static/js/main.d07f3fb9.chunk.js"
+  },
+  {
+    "revision": "ea49c99185fd1f25fa75",
+    "url": "/InstaZZ-front/static/js/runtime-main.a3a3cb5f.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/InstaZZ-front/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "3a50b41b44c7dcc27e9a7d147cc97acc",
+    "url": "/InstaZZ-front/static/media/logo_transparent.3a50b41b.png"
+  }
+]);
